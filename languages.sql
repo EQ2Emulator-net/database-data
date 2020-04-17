3339db6167208b3b99ce145dda5ba20ff3164a11
Cynnar <jacob0305@gmail.com>
2020-04-17 03:23:09 -0400

    Initial Commit

---
+++ languages.sql
@@ -0,0 +1,27 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+LOCK TABLES `languages` WRITE;
+/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
+INSERT INTO `languages` (`id`, `language`) VALUES (42,'Ancient Draconic'),(11,'Antonican'),(32,'Argot'),(7,'Ayr\'Dal'),(24,'Chaos Tongue'),(29,'Death\'s Whisper'),(38,'Di\'Zokian'),(19,'Draconic'),(27,'Druzaic'),(3,'Dwarven'),(4,'Erudian'),(35,'Faerlie'),(23,'Fayefolk'),(16,'Feir\'Dal'),(43,'Fleshless Tongue'),(39,'Froak'),(18,'Gnollish'),(6,'Gnomish'),(20,'Goblish'),(36,'Gorwish'),(5,'Guktan'),(44,'Gymy'),(1,'Halasian'),(13,'Kerran'),(9,'Koada\'Dal'),(25,'Krombral'),(10,'Lucanic'),(14,'Oggish'),(17,'Orcish'),(26,'Ratongan'),(37,'Sathirian'),(30,'Screechsong'),(12,'Sebilisian'),(33,'Serilian'),(40,'Shisik'),(8,'Stout'),(41,'Sul\'Dal'),(2,'Thexian'),(21,'Thulian'),(34,'Tik-Tok'),(28,'Uruvanian'),(31,'Volant'),(22,'Words of Shade'),(15,'Ykeshan');
+/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
